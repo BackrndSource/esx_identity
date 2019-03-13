c45e1e8328c7e4b1e8952070b29e37b1b5228fa9
BackrndSource <back.rndsource@gmail.com>
2019-03-13 23:45:47 +0000

    Create en.lua

---
+++ locale/en.lua
@@ -0,0 +1,9 @@
+Locales['en'] = {
+  ['more_than_two_spaces'] = 'your name contains more than two spaces',
+  ['must_capital_letter'] = 'your name must start with a capital letter.',
+  ['characters_not_allowed'] = 'your player name contains special characters that are not allowed on this server.',
+  ['name_lenght_not_allowed'] = 'your player name is either too short (<2) or too long (>25)',
+  ['unacceptable_player_height'] = 'unacceptable player height!',
+  ['invalid_date_birth'] = 'invalid date of birth!',
+  ['must_create_first_character'] = '^1You must create your first character in order to play'
+}
