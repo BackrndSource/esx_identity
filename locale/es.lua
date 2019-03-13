2c4eb3cc2e3abca86978ce33713bff1da667b63d
BackrndSource <back.rndsource@gmail.com>
2019-03-13 23:46:57 +0000

    Create es.lua

---
+++ locale/es.lua
@@ -0,0 +1,9 @@
+Locales['es'] = {
+  ['more_than_two_spaces'] = 'tu nombre de jugador contiene más de dos espacios',
+  ['must_capital_letter'] = 'tu nombre de jugador debe empezar por mayúscula',
+  ['characters_not_allowed'] = 'tu nombre de jugador contiene carácteres que no son aceptados',
+  ['name_lenght_not_allowed'] = 'tu nombre de jugador es demasiado largo (>25) o demasiado corto (<2)',
+  ['unacceptable_player_height'] = 'altura del personaje no válida!',
+  ['invalid_date_birth'] = 'fecha de nacimiento no válida!',
+  ['must_create_first_character'] = '^1Debes crear tu primer perfil para jugar'
+}
